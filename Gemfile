54a926b03ada61ac2cf4fcb957b12a2586b2df1b
Anthony Franco <arfranco@Anthonys-MacBook-Pro.local>
2015-06-07 17:15:12 -0400

    added HTML and CSS as well as initial server/API

--- Gemfile
+++ Gemfile
@@ -3,9 +3,9 @@ source 'https://rubygems.org'
 # Specify your gem's dependencies in etsiest.gemspec
 gemspec
 
-gem ‘sinatra’
-gem ‘etsy’
+gem 'sinatra'
+gem 'etsy'
 
 group :development do
- gem ‘pry’
+ gem 'pry'
 end
--- lib/etsiest.rb
+++ lib/etsiest.rb
@@ -1,5 +1,27 @@
+require 'sinatra'
+require 'pry'
+require 'etsy'
 require "etsiest/version"
 
+Etsy.protocol = "https"
+Etsy.api_key = ENV['ETSY_KEY']
+request = Etsy.request_token
+Etsy.verification_url
+
+
 module Etsiest
-  # Your code goes here...
+  class App < Sinatra::Base
+
+    get '/' do 
+      if params[:q]
+        response = Etsy::Request.get('/listings/active', :includes => ['Images', 'Shop'], :keywords => params[:q])
+        @results = JSON.parse(response.body)['results']
+      end
+      erb :index
+    end
+
+    run! if app_file == $0
+  end
 end
+
+binding.pry
